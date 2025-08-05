1780aa8b935f891bf9b900c77314e17bc13450ea
Aidan O'Brien <aob55992@gmail.com>
2025-08-05 13:47:36 -0400

    getting this so no one can steal my db lmao

--- run_dumps.js
+++ run_dumps.js
@@ -13,11 +13,11 @@ if (args.length > 0) {
   }
 
 const connection = mysql.createConnection({
-    host: 'interchange.proxy.rlwy.net',
-    user: 'root',
-    password: 'czbdENAQEFTdOXMMzhPmIaSNqGdSaKlF',
-    database: 'railway',
-    port: 23462,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     multipleStatements: true // Important for dump files
 });
 
